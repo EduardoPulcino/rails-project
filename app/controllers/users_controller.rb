60f19ee517099e10cfb2cde7e2f85de7a555300b
EduardoPulcino <142759102+EduardoPulcino@users.noreply.github.com>
2024-11-21 20:01:40 -0300

    Add crud user (#1)

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,52 @@
+class UsersController < ApplicationController
+	skip_before_action :verify_authenticity_token, only: :create
+	before_action :set_user, only: %i[show edit update destroy]
+	
+	def index 
+		@users = User.all
+		render json: @users
+	end
+
+	def show
+		render json: @user
+	end
+
+	def new
+		@user = User.new
+	end
+
+	def create
+		@user = User.new(user_params)
+		@user.role = 'USER'
+		if @user.save
+			redirect_to @user, notice: 'User created!'
+		else
+			render :new
+		end
+	end
+
+	def edit
+	end
+
+	def update
+		if @user.update(user_params)
+			redirect_to @user, notice: 'User updated!'
+		else
+			render :edit
+		end
+	end
+
+	def destroy
+		@user.destroy
+	end
+
+	private
+
+	def set_user
+		@user = User.find(params[:id])
+	end
+
+	def user_params
+		params.require(:user).permit(:name, :email, :password, :phone, :role, :photo)
+	end
+end
