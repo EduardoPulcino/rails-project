3b08b329065fbdf2676b6907ae56a528e98675c8
EduardoPulcino <eduardo.pulcino@sptech.school>
2025-01-11 15:16:23 -0300

    Configura RSpec e FactoryBot

---
+++ spec/factories/users.rb
@@ -0,0 +1,15 @@
+FactoryBot.define do
+	factory :user do
+		name { 'Eduardo' }
+		email { 'eduardo@gmail.com' }
+		password { '123456' }
+		phone { '11997828712' }
+		role { :user }
+
+		trait :with_photo do
+      after(:create) do |user|
+        user.photo.attach(io: File.open(Rails.root.join('spec', 'fixtures', 'avatar.png')), filename: 'avatar.png', content_type: 'image/png')
+      end
+    end
+	end
+end
