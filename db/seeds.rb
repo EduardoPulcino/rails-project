d1e32e080c4eceb828491578a4bec70329b943e9
EduardoPulcino <eduardo.pulcino@sptech.school>
2025-03-28 19:40:30 -0300

    Add admin and user in seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,23 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+admin = User.create!(
+  name: 'Admin User',
+  email: 'admin@example.com',
+  password: 'password123',
+  password_confirmation: 'password123',
+  phone: '1234567890',
+  role: :admin
+)
+
+user = User.create!(
+  name: 'Regular User',
+  email: 'user@example.com',
+  password: 'password123',
+  password_confirmation: 'password123',
+  phone: '0987654321',
+  role: :user
+)
+
+
