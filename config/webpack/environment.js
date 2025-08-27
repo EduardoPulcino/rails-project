a917f76810ab8d44ac75d04abde42b0996e4a3f1
EduardoPulcino <eduardo.pulcino@sptech.school>
2025-08-26 23:39:02 -0300

    Ajusta config

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,3 +1,7 @@
 const { environment } = require('@rails/webpacker')
 
+const babelLoader = environment.loaders.get('babel')
+
+babelLoader.exclude = /node_modules\/(?!(@hotwired))/ 
+
 module.exports = environment
