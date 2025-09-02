3b9a99131024bac761b51bd449ca57b9066d503c
EduardoPulcino <eduardo.pulcino@sptech.school>
2025-09-02 01:19:06 -0300

    Finaliza tela login

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -24,4 +24,8 @@ class Users::SessionsController < Devise::SessionsController
   def configure_sign_in_params
     devise_parameter_sanitizer.permit(:sign_in, keys: [:name, :photo, :phone])
   end
+
+  def after_sign_in_path_for(resource)
+    new_budget_path
+  end
 end
