c5d54db18e8f862ceb8797e431a44da903022d98
EduardoPulcino <eduardo.pulcino@sptech.school>
2025-09-06 00:16:19 -0300

    Corrige campo telefone

--- app/javascript/controllers/mask_controller.js
+++ app/javascript/controllers/mask_controller.js
@@ -7,15 +7,22 @@ export default class extends Controller {
   }
 
   mask(event) {
-    let value = event.target.value.replace(/\D/g, "") // só números
-    value = value.slice(0, 11) // máximo 11 dígitos
+    const input = event.target
+    let cursor = input.selectionStart
+
+    let value = input.value.replace(/\D/g, "")
+    value = value.slice(0, 11)
 
     if (value.length > 2 && value.length <= 6) {
       value = `(${value.slice(0, 2)}) ${value.slice(2)}`
-    } else if (value.length > 6) {
+    } else if (value.length > 6 && value.length <= 10) {
+      value = `(${value.slice(0, 2)}) ${value.slice(2, 6)}-${value.slice(6)}`
+    } else if (value.length > 10) {
       value = `(${value.slice(0, 2)}) ${value.slice(2, 7)}-${value.slice(7)}`
     }
 
-    event.target.value = value
+    input.value = value
+
+    input.setSelectionRange(value.length, value.length)
   }
 }
