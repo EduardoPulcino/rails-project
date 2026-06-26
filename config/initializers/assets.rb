c339f59bf48918c93280ef9e21d23ee2d4d6755f
EduardoPulcino <eduardo.pulcino@sptech.school>
2026-06-25 22:37:13 -0300

    Add assets to precompile

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -11,4 +11,36 @@ Rails.application.config.assets.paths << Rails.root.join('node_modules')
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in the app/assets
 # folder are already added.
-# Rails.application.config.assets.precompile += %w( admin.js admin.css )
+Rails.application.config.assets.precompile += %w[
+  aniversario.webp
+  avatar.png
+  BannerInicial.svg
+  BannerLogin.svg
+  Background\ -\ Login.svg
+  bolos.jpeg
+  casamento.jpg
+  CastrilloEventos.png
+  clock.svg
+  coffebreak.jpeg
+  decoracao4.jpg
+  debutante.jpg
+  doces.webp
+  eventoLove.jpg
+  imgDecoracaoBanner.jpg
+  imgDecoracaoBanner2.jpg
+  imgDecoracaoBanner3.jpg
+  imgDecoracaoBanner4.jpg
+  imgDecoracaoBanner5.jpg
+  imgDecoracaoBanner6.jpg
+  imgDecoracaoBanner7.jpg
+  imgPerfil.png
+  lanches.webp
+  loading-gif.gif
+  logoBranca.png
+  Matriz\ \(1\).png
+  Pencil.png
+  piscina.jpg
+  Salvagos.jpeg
+  teste.jpeg
+  VectorVoltar.svg
+]
