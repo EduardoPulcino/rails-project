e2262411e4ce282d769820b2f7541fe471830871
EduardoPulcino <eduardo.pulcino@sptech.school>
2026-06-25 22:19:55 -0300

    fix deploy config

--- config/setup_app_production.sh
+++ config/setup_app_production.sh
@@ -1,13 +1,15 @@
-#!/bin/sh
+#!/bin/bash
+
+set -e
 
 echo "-> PORT is: $PORT"
 echo "-> RAILS_ENV is: $RAILS_ENV"
 
 echo "-> Removing stale server.pid..."
-if [ -f tmp/pids/server.pid ]; then
-  rm tmp/pids/server.pid
-fi
+rm -f tmp/pids/server.pid
 
 echo "-> Running migrations..."
-bundle exec rails db:migrate 2>&1
-echo "-> Migration exit code: $?"
+bundle exec rails db:migrate
+
+echo "-> Starting Rails..."
+exec bundle exec rails s -p $PORT -b 0.0.0.0
